4b0515ade59eff70b5415a2d6a6aca1250cbe427
pausabat99 <pau.sabat.marti@estudiantat.upc.edu>
2022-11-22 18:16:51 +0100

    eliminat part del dom i afegit titol mètriques

--- scripts/content.js
+++ scripts/content.js
@@ -1,13 +1,31 @@
 setTimeout(() => {
     execute();
-  }, "7000")
+  }, "6000")
 
 
 function execute() {
-    const collection = document.getElementsByClassName("single-project-intro")[0];
-    var spn = document.createElement("a");
-    spn.textContent = "METRICS!!!";
-    collection.parentNode.insertBefore(spn, collection.nextSibling);
+    const title = document.getElementsByClassName("description")[0];
+
+    var metrics = document.createElement("div");
+    var button = document.createElement("button");
+    button.setAttribute("id", "metricsButton");
+    button.innerHTML = "METRICS";
+    metrics.appendChild(button);
+
+    title.parentNode.insertBefore(metrics, title.nextSibling);
+
+    button.onclick = function() {
+      var timeline = document.getElementsByClassName("timeline")[0];
+      var firstChild = timeline.children[0];
+      firstChild.remove();
+
+      var div = document.createElement("div");
+      var html =
+      '<h1 id="h1">Selecciona les mètriques</h1>';
+      div.innerHTML = html;
+
+      timeline.appendChild(div);
+    }
 }
 
 
