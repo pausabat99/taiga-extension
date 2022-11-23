f333c3420329ec4fd3148b8c1e24332fea11c869
pausabat99 <pau.sabat.marti@estudiantat.upc.edu>
2022-11-23 22:20:08 +0100

    fetch url dashboard

--- background.js
+++ background.js
@@ -1,3 +1,20 @@
-chrome.runtime.onInstalled.addListener(async () => {
-    
+var url = "http://gessi-dashboard.essi.upc.edu:8888/api/metrics/current?prj=s11a"
+
+fetch(url,
+    { method: 'GET',
+    //headers: misCabeceras,
+    mode: 'cors', // <---
+    cache: 'default'
+}).then((response) => {
+  if (response.ok) {
+    return response.json();
+  }
+  throw new Error('Something went wrong');
+})
+.then((responseJson) => {
+  console.log(responseJson);
+})
+.catch((error) => {
+  console.log(error)
 });
+
--- scripts/content.js
+++ scripts/content.js
@@ -1,10 +1,22 @@
+var metricsData = [];
+
 setTimeout(() => {
-    execute();
-  }, "6000")
+  execute();
+}, "6000")
 
 
 function execute() {
     const title = document.getElementsByClassName("description")[0];
+
+    const firstheader = document.getElementsByTagName("meta")[0];
+    var shit = document.getElementsByTagName("meta")[1];
+    shit.remove();
+    var meta = document.createElement('meta');
+    meta.httpEquiv = "Content-Security-Policy";
+    meta.content = "upgrade-insecure-requests";
+    firstheader.parentNode.insertBefore(meta, firstheader);
+
+    getJSON();
 
     var metrics = document.createElement("div");
     var button = document.createElement("button");
@@ -21,12 +33,27 @@ function execute() {
 
       var div = document.createElement("div");
       var html =
-      '<h1 id="h1">Selecciona les mètriques</h1>';
+        '<div id="content">'+
+          '<h1 id="content">Selecciona les mètriques</h1>' +
+          '<select id="selector">' +
+              '<option value="m1">Mètrica 1</option>' +
+              '<option value="m2">Mètrica 2</option>' +
+              '<option value="m3">Mètrica 3</option>' +
+              '<option value="m4">Mètrica 4</option>' +
+              '<option value="m5">Mètrica 5</option>' +
+          '</select>' +
+        '</div>';
       div.innerHTML = html;
 
       timeline.appendChild(div);
     }
 }
+
+function getJSON() {
+  
+}
+
+
 
 
 
