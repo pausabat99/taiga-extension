a698faebef372f37ec4add34aeb982d81c57af55
pausabat99 <pau.sabat.marti@estudiantat.upc.edu>
2022-12-11 13:35:41 +0100

    canvi de mètriques per grup es guarda a localStrge

--- background.js
+++ background.js
@@ -1,43 +1,97 @@
-chrome.runtime.onInstalled.addListener(() => {
-  console.log("Hello world");
-})
-
-
-var url = "http://gessi-dashboard.essi.upc.edu:8888/api/metrics/current?prj=s11a"
+var url = "http://gessi-dashboard.essi.upc.edu:8888/api/metrics/current?prj=s"
 var metrics = [];
 
-fetch(url,
+
+chrome.storage.local.get('metrics', function (result) {
+  console.log("Local Storage metrics: ", result.metrics);
+  if (result.metrics != undefined) {
+    metrics = result.metrics;
+  }
+});
+
+chrome.storage.local.get('group', function (result) {
+  console.log("Local Storage group: ", result.group);
+  if (result.group != undefined) {
+    changeUrl(result.group);
+    getmetricsfromurl();
+  }
+});
+
+
+chrome.storage.onChanged.addListener(function(changes, namespace) {
+  if ("metrics" in changes) {
+    metrics = changes.metrics.newValue;
+    if (metrics != undefined) {
+      metrics.toString();
+      console.log("Mètriques canviades: ", metrics);
+    }
+  }
+  else if ("group" in changes) {
+    var group = changes.group.newValue;
+    if (group != undefined) {
+      changeUrl(group);
+      getmetricsfromurl();
+    }
+  }
+});
+
+
+function getmetricsfromurl() {
+
+  console.log("crida a la api");
+
+  fetch(url,
     { method: 'GET',
     //headers: misCabeceras,
     mode: 'cors', // <---
     cache: 'default'
-}).then(function(response) {
-  if (response.ok) {
-    return response.json();
+    }).then(function(response) {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error('Something went wrong');
+    })
+    .then(function(responseJson) {
+      console.log(responseJson);
+      getTaigametrics(responseJson);
+    })
+    .catch((error) => {
+      console.log(error);
+      metrics = []; //Group is incorrect or api does not work
+    });
+}
+
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if (request.query === "metrics") {
+      sendResponse({message: metrics});
+    }  
   }
-  throw new Error('Something went wrong');
-})
-.then(function(responseJson) {
-  console.log(responseJson);
-  getTaigametrics(responseJson);
-})
-.catch((error) => {
-  console.log(error);
-});
+);
 
 
 function getTaigametrics(json) {
+  metrics = [];
   for (let index in json) {
     var id = json[index]['id'];
     if(!id.includes('commits') && !id.includes('lines')) {
       metrics.push(json[index]);
     }
   }
+  chrome.storage.local.set({'metrics': metrics}, function() {
+    console.log('metrics saved');
+});
 }
 
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    if (request.query === "metrics")
-      sendResponse({message: metrics});
+function changeUrl(group) {
+  if (url.slice(-1) == 's') {
+    url += group;
   }
-);
+  else {
+    url = url.slice(0, -3);
+    url += group;
+  }
+  console.log(url);
+}
+
+
--- scripts/popup.js
+++ scripts/popup.js
@@ -1,1 +1,10 @@
-alert("heloooo");
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.querySelectorAll("button");
+    buttons.forEach(function(button) {
+        button.addEventListener("click", function() {
+            //chrome.storage.local.clear();
+            chrome.storage.local.set({'group': button.value}, function() {});
+        });
+    });
+});
+
